08714880f2bcd696fb2a2e471f1c58480550087f
cesaraugustooo <cesaraugusto23121989@gmail.com>
2026-01-24 18:03:09 -0300

    feat: adiciona fillable para campos de vendedores

--- app/Models/vandedores.php
+++ app/Models/vandedores.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class vandedores extends Model
+class vendedores extends Model
 {
-    //
+    public $fillable = ['user_id', 'loja_nome', 'atividade'];
 }
