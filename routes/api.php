87e8bbfecaa207097ea9be88f4d31d94cbbda643
cesaraugustooo <cesaraugusto23121989@gmail.com>
2026-01-24 19:32:19 -0300

    feat: adiciona rota para registro

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+require base_path('routes/auth/auth.php');
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
