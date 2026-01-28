3a032b95212c1f69d9fa61c5fcc01d9d803d4dc0
cesaraugustooo <cesaraugusto23121989@gmail.com>
2026-01-28 17:19:52 -0300

    feat: adiciona rotas de produtos

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 require base_path('routes/auth/auth.php');
 require base_path('routes/vendedores/vendedores.php');
+require base_path('routes/produtos/produtos.php');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
