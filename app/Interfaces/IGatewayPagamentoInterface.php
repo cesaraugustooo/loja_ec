ac9eeb8a8bfc5bb3ac597f82761eaab75081e21f
cesaraugustooo <cesaraugusto23121989@gmail.com>
2026-01-31 02:01:59 -0300

    feat: adiciona novo parametro "produto"

--- app/Interfaces/IGatewayPagamentoInterface.php
+++ app/Interfaces/IGatewayPagamentoInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Interfaces;
+
 interface IGatewayPagamentoInterface {
-    public function setPagamento($pedido): object;
+    public function setPagamento($pedido, $produto): string;
 }
