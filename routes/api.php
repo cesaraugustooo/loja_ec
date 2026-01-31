4fbb138d7352acfcf0ab978e68c19a87c99e6f85
cesaraugustooo <cesaraugusto23121989@gmail.com>
2026-01-31 00:33:38 -0300

    feat: importa rotas de pedidos ao arquivo de rotas do laravel

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,25 @@
 <?php
 
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 require base_path('routes/auth/auth.php');
 require base_path('routes/vendedores/vendedores.php');
 require base_path('routes/produtos/produtos.php');
+require base_path('routes/pedidos/pedidos.php');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Route::get('/pagamento-teste',[ProdutoController::class, 'teste']);
+
+// Route::get('/success',function(){
+//     return response()->json(['message' => 'success']);
+// })->name('success');
+
+
+// Route::get('/cancel',function(){
+//     return response()->json(['message' => 'cancel']);
+// })->name('cancel');
