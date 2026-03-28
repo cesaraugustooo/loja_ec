950d1f84786d05a852c219f5840c2ff0948439d6
Cesar <cesaraugusto23121989@gmail.com>
2026-03-28 13:20:52 -0300

    feat: adiciona metodo de listar e atualizar categorias

--- app/Services/CategoriaService.php
+++ app/Services/CategoriaService.php
@@ -3,24 +3,38 @@
 namespace App\Services;
 
 use App\Interfaces\ICategoriaInterface;
+use App\Models\Categoria;
 
-class CategoriaService {
+class CategoriaService
+{
     public function __construct(
         private ICategoriaInterface $categoriaRepository
     ) {}
 
-    public function create($dados) {
+    public function listar()
+    {
+        return $this->categoriaRepository->index();
+    }
+
+    public function create($dados)
+    {
         $categoria = $this->categoriaRepository->create($dados);
 
         return $categoria;
     }
 
-    public function view($id) {
+    public function view($id)
+    {
         return $this->categoriaRepository->view($id);
     }
 
-            public function atualizar(int $id, array $dados)
+    public function atualizar(Categoria $categoria, array $dados)
     {
-        return $this->categoriaRepository->update($id, $dados);
+        return $this->categoriaRepository->update($categoria, $dados);
+    }
+
+    public function deletar(Categoria $categoria)
+    {
+        return $this->categoriaRepository->destroy($categoria);
     }
 }
