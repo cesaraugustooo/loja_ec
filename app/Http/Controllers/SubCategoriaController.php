9e2d24013fb64fe337e3793c9120206bd1cbc961
Cesar <cesaraugusto23121989@gmail.com>
2026-03-28 13:38:35 -0300

    fix: remove dependencia de user_id em subcategorias

--- app/Http/Controllers/SubCategoriaController.php
+++ app/Http/Controllers/SubCategoriaController.php
@@ -1,24 +1,25 @@
 <?php
+
 namespace App\Http\Controllers;
 
-use App\Http\Requests\SubCategoriaRequest;
 use App\Services\SubCategoriaService;
 use App\Models\Sub_categoria;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
+use App\Http\Requests\SubCategoriaRequest;
 
 class SubCategoriaController extends Controller
 {
     use AuthorizesRequests;
-    public function index(SubCategoriaService $services) {
-        return response()->json($services->listar(), 200);
+    public function index(SubCategoriaService $services)
+    {
+        return response()->json($services->index(), 200);
     }
 
-    public function store (SubCategoriaRequest $request, SubCategoriaService $service)
+    public function store(SubCategoriaRequest $request, SubCategoriaService $service)
     {
         $validated = $request->validated();
-        $validated = ['user_id'] = $request->user()->id;
 
         $sub_categoria = $service->create($validated);
 
@@ -41,11 +42,11 @@ class SubCategoriaController extends Controller
     }
 
     public function destroy(Sub_categoria $sub_categoria, SubCategoriaService $service)
-{
-    $this->authorize('delete', $sub_categoria);
+    {
+        $this->authorize('delete', $sub_categoria);
 
-    $service->deletar($sub_categoria);
+        $service->deletar($sub_categoria);
 
-    return response()->json([], 204);
-}
+        return response()->json([], 204);
+    }
 }
