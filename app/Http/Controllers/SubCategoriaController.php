a545196d53a756eb069524628dcbcb7dd0fa28fe
EnzoZanardi16 <enzozanardi48@gmail.com>
2026-03-28 12:44:19 -0300

    feat: adiciona Request e Controller das sub categorias

---
+++ app/Http/Controllers/SubCategoriaController.php
@@ -0,0 +1,51 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SubCategoriaRequest;
+use App\Services\SubCategoriaService;
+use App\Models\Sub_categoria;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+
+class SubCategoriaController extends Controller
+{
+    use AuthorizesRequests;
+    public function index(SubCategoriaService $services) {
+        return response()->json($services->listar(), 200);
+    }
+
+    public function store (SubCategoriaRequest $request, SubCategoriaService $service)
+    {
+        $validated = $request->validated();
+        $validated = ['user_id'] = $request->user()->id;
+
+        $sub_categoria = $service->create($validated);
+
+        return response()->json($sub_categoria, 201);
+    }
+
+    public function view(Sub_categoria $sub_categoria, SubCategoriaService $service)
+    {
+        return response()->json($sub_categoria->load(['categorias']), 200);
+    }
+
+    public function update(SubCategoriaRequest $request, SubCategoriaService $service, Sub_categoria $sub_categoria)
+    {
+
+        $this->authorize('update', $sub_categoria);
+
+        $sub_categoria = $service->atualizar($sub_categoria, $request->validated());
+
+        return response()->json($sub_categoria, 200);
+    }
+
+    public function destroy(Sub_categoria $sub_categoria, SubCategoriaService $service)
+{
+    $this->authorize('delete', $sub_categoria);
+
+    $service->deletar($sub_categoria);
+
+    return response()->json([], 204);
+}
+}
