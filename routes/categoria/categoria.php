3b072984ff2a8b26425c78abb584fa8f22319ad6
EnzoZanardi16 <enzozanardi48@gmail.com>
2026-04-11 00:36:34 -0300

    fix: arruma rota da categoria

--- routes/categoria/categoria.php
+++ routes/categoria/categoria.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\CategoriaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/categoria",[CategoriaController::class, "view"])->middleware('auth:sanctum');
-Route::post("/categoria/{categoria}",[CategoriaController::class, "store"])->middleware('auth:sanctum');
+Route::post("/categoria",[CategoriaController::class, "store"])->middleware('auth:sanctum');
