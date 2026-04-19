21acad6ca938b8dc761610c08401c768f461ee10
EnzoZanardi16 <enzozanardi48@gmail.com>
2026-04-19 16:12:14 -0300

    feat: adiciona rota index nas categorias

--- routes/categoria/categoria.php
+++ routes/categoria/categoria.php
@@ -3,5 +3,6 @@
 use App\Http\Controllers\CategoriaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/categoria",[CategoriaController::class, "view"])->middleware('auth:sanctum');
+Route::get("/categoria",[CategoriaController::class, "index"])->middleware('auth:sanctum');
+Route::get("/categoria/{id}",[CategoriaController::class, "view"])->middleware('auth:sanctum');
 Route::post("/categoria",[CategoriaController::class, "store"])->middleware('auth:sanctum');
