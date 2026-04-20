0b653390b5ada42e8ad1d603bcf127fa9d90bb7c
EnzoZanardi16 <enzozanardi48@gmail.com>
2026-04-20 15:06:51 -0300

    feat: rota index de categoria

--- routes/categoria/categoria.php
+++ routes/categoria/categoria.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\CategoriaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/categoria",[CategoriaController::class, "index"])->middleware('auth:sanctum');
+Route::get("/categoria",[CategoriaController::class, "index"]);
 Route::get("/categoria/{id}",[CategoriaController::class, "view"])->middleware('auth:sanctum');
 Route::post("/categoria",[CategoriaController::class, "store"])->middleware('auth:sanctum');
